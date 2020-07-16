f48475df2872bb84a0564413ca7a0053f32e68e3
Shadow15510 <antoine.royer31@orange.fr>
2020-07-16 18:31:42 +0200

    First commit

---
+++ main.py
@@ -0,0 +1,23 @@
+# --------------------------------------------------
+# Odyssée (Version dev)
+# by LeRoiDesKiwis and Sha-Chan~
+# last version released on the … of … 2020
+#
+# Code provided with licence (CC BY-NC-SA 4.0)
+# for more information about licence :
+# https://creativecommons.org/licenses/by-nc-sa/4.0/
+# --------------------------------------------------
+
+from piscord import Handler
+
+odyssee = Handler("", "+")
+
+@bot.event("on_ready")
+def ready(ready):
+  print(f"{ready.user.name} online and connected.")
+
+@bot.event("on_message")
+def message(messgage):
+   answer = rp(message.author.nick, message.author.id, message.content[1:])
+    
+
